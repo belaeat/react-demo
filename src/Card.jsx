08abd9f8292520fc024029b459c040e36b35c129
belaeat <omyhossain@gmail.com>
2024-10-28 23:08:25 +0200

    week2 update

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,6 +4,7 @@ function Card(props) {
       <p>{props.firstName}</p>
       <p>Title: {props.title}</p>
       <p>Age: {props.age}</p>
+      <p>Favourite Animal: {props.favAnimal}</p>
     </div>
   );
 }
